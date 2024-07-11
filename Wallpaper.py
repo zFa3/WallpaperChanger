397aa9d5134e22de7b3f9d8ed22f5f61dec85deb
zFa3 <153332084+zFa3@users.noreply.github.com>
2024-07-11 10:34:01 -0400

    added some more functionality
    buggy, path variable doesnt work

--- Wallpaper.py
+++ Wallpaper.py
@@ -1,20 +1,18 @@
-#!usr/bin/env python3
+#!usr/bin/env/ python3
 
-# import stuff
-import os
+# import all the packages
+# https://docs.python.org/3/library/ctypes.html
 import ctypes
+import os
 
-path = "ENTER PATH HERE"
-path = "C:\\Users\user\downloads\images"
+try:
+    # https://docs.python.org/3/library/os.html
+    images = (os.listdir(r"C:\Users\User\Downloads\Images"))
+    image = "C:\\Users\\User\\Downloads\\Images\\testimg.png"
+    print(images)
 
-# random functions; might need later
-# useful resource: https://docs.python.org/3/library/os.html
-
-# os listdir gets directory
-images = (os.listdir("C:\Users\User\Downloads\Images"))
-images = (os.listdir(path,))
-
-print(f"Number of items found in directory - {len(images)}")
-
-ctypes.windll.user32.SystemParametersInfoA(0, 0, "", 0)
-# os.copy_file_range(src, dst, count, offset_src=None, offset_dst=None)
+    print(f"Number of items found in directory - {len(images)}")
+    print(f"Image used - {image}")
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, (image), 0)
+except:
+    ctypes.windll.user32.SystemParametersInfoA(20, 0, r"", 0)
