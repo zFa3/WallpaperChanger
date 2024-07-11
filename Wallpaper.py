ace401d2f5cc532f53ce73def157ada5f3632cfe
zFa3 <153332084+zFa3@users.noreply.github.com>
2024-07-11 10:26:24 -0400

    Update Wallpaper.py

--- Wallpaper.py
+++ Wallpaper.py
@@ -9,7 +9,12 @@ path = "C:\\Users\user\downloads\images"
 
 # random functions; might need later
 # useful resource: https://docs.python.org/3/library/os.html
-os.fsencode()
-os.get_exec_path(None)
+
+# os listdir gets directory
+images = (os.listdir("C:\Users\User\Downloads\Images"))
+images = (os.listdir(path,))
+
+print(f"Number of items found in directory - {len(images)}")
+
 ctypes.windll.user32.SystemParametersInfoA(0, 0, "", 0)
 # os.copy_file_range(src, dst, count, offset_src=None, offset_dst=None)
